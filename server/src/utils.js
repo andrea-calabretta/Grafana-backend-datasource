7eaf6eb8e2455a2ef5b039c98fee030e93761eca
andrea-calabretta <andreacalabretta0@gmail.com>
2021-10-28 22:40:26 +0200

    useless files deleted

--- server/src/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.randomIntFromInterval = void 0;
-function randomIntFromInterval(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-}
-exports.randomIntFromInterval = randomIntFromInterval;
